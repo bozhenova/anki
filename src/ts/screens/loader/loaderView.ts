a7b82cca8baea240b219f85a123c2bf88eb85e11
Margo <margo.bozhenova@gmail.com>
2020-06-01 01:41:39 +0300

    Add Loader class and normalize

---
+++ src/ts/screens/loader/loaderView.ts
@@ -0,0 +1,14 @@
+import AbstractView from "../../abstractView";
+
+export default class LoaderView extends AbstractView {
+  constructor() {
+    super();
+  }
+
+  get template() {
+    return `<div class="loader">
+          <p class="loader__text">Loading data...</p>
+          <div class="loader__icon"></div>
+          </div>`;
+  }
+}
